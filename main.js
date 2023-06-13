936a4ca1e317df53a577496a302e50d6009413d0
Damián Roháček <rohacekdamian@seznam.cz>
2023-06-13 12:20:30 +0200

    bot kolize, taknějak to funguje

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var playerWidth = 50;
 var tile = 50;
 var friction = 0;
 var gravity = 0;
-var ground = 350;
+var ground = 500;
 var onGround = true;
 
 var mezernikDown = false;
@@ -54,6 +54,9 @@ function player() {
     if (playerY<0) {
         playerY=0;
     }
+
+    oldPlayerX = playerX;
+    oldPlayerY = playerY;
 }
 
 function KeyDown(event) {
@@ -97,16 +100,16 @@ function collision() {
     var botLeftY = ((playerY + playerHeight +0.001)-((playerY + playerHeight +0.001) % tile)) / tile;
     var botLeftTile = tilemapV[botLeftY][botLeftX];
 
-    var botLeftX2 = ((playerX-(playerX % tile))- 0.001) / tile;
-    var botLeftY2 = ((playerY + playerHeight - 0.001)-((playerY + playerHeight) % tile)) / tile;
+    var botLeftX2 = ((playerX-(playerX % tile))) / tile;
+    var botLeftY2 = ((playerY + playerHeight - 0.001)-((playerY + playerHeight - 0.001) % tile)) / tile;
     var botLeftTile2 = tilemapV[botLeftY2][botLeftX2];
 
     var botRightX = ((playerX + playerWidth)-((playerX + playerWidth) % tile)) / tile;
     var botRightY = ((playerY + playerHeight +0.001)-((playerY + playerHeight +0.001) % tile)) / tile;
     var botRightTile = tilemapV[botRightY][botRightX];
 
-    var botRightX2 = ((playerX + playerWidth)-((playerX + playerWidth + 0.001) % tile)) / tile;
-    var botRightY2 = ((playerY + playerHeight)-((playerY + playerHeight - 0.001) % tile)) / tile;
+    var botRightX2 = ((playerX + playerWidth)-((playerX + playerWidth) % tile)) / tile;
+    var botRightY2 = ((playerY + playerHeight - 0.001)-((playerY + playerHeight - 0.001) % tile)) / tile;
     var botRightTile2 = tilemapV[botRightY2][botRightX2];
 
     var topRightX = ((playerX + playerWidth)-((playerX + playerWidth) % tile)) / tile;
@@ -115,6 +118,7 @@ function collision() {
     
     if (botLeftTile != 0 || botRightTile != 0) {
         if(!topCollision()){
+            
             if ((topLeftTile > 2 && topLeftTile < 7) || (botLeftTile2 > 2 && botLeftTile2 <7)) {
                 rightCollision();
             } else if ((topRightTile == 2 || topRightTile == 7) || (botRightTile2 > 2 && botRightTile2 <7)) {
@@ -122,26 +126,27 @@ function collision() {
             }
         }
     }
-    console.log(botLeftTile2);
-    /*if (botLeftTile != 0 || botRightTile != 0) {
-        topCollision();
-    } else if ((topLeftTile > 2 && topLeftTile < 7) || (botLeftTile2 > 2 && botLeftTile2 <7)) {
-        rightCollision();
-    } else if (topRightTile == 2 || topRightTile == 7) {
-        leftCollision();
-    }*/
+    if (topRightTile == 8 || topLeftTile == 8) {
+        botCollision();
+    }
+    
+    
     if (botLeftTile == 0 && botRightTile == 0) { onGround = false;}
     
-
-
+    function botCollision() {
+        playerY += tile - (playerY % tile);
+        console.log("negro");
+    }
     function topCollision() {
         if (gravity > 0) {
         
             if (botLeftTile == 1 || (botLeftTile > 4 && botLeftTile<8)) {
-                playerY -= (playerY+playerHeight) % 50;
-                gravity = 0;
-                onGround = true;
-                return true;
+                if (botLeftY*50 > oldPlayerY + playerHeight || botRightY*50 > oldPlayerY + playerHeight) {
+                    playerY -= (playerY+playerHeight) % 50;
+                    gravity = 0;
+                    onGround = true;
+                    return true;
+                }  
             }
             if (botRightTile == 1 ||(botRightTile > 4 && botRightTile<8)) {
                 playerY -= (playerY+playerHeight) % 50;
