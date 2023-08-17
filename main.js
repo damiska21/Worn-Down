d1cef9019fb7f7d3776baa1aabc167f08987af46
Damián Roháček <rohacekdamian@seznam.cz>
2023-08-17 20:53:12 +0200

    veškerý kolize + coyote time

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 var canvas = document.createElement("canvas");canvas.width = window.innerWidth-20;canvas.height = window.innerHeight - 20;document.body.appendChild(canvas);var c = canvas.getContext("2d");
 
 var playerX = 100;
-var playerY = 150;
+var playerY = 100;
 var oldPlayerX = playerX; var oldPlayerY = playerY;
 var playerHeight = 75;
 var playerWidth = 50;
+
 var tile = 50;
+
 var friction = 0;
 var gravity = 0;
 var ground = 500;
 var onGround = true;
+
+var coyoteTime = false;
+var coyoteTimeTick = 0;
 
 var mezernikDown = false;
 var leftDown = false;
@@ -45,7 +50,11 @@ function player() {
         gravity += 1.2;
         playerY += gravity;
     }
-    if (mezernikDown && onGround) {
+    if (mezernikDown && (onGround || coyoteTime)) {
+        if (coyoteTime) {
+            coyoteTime = false;
+        }
+        console.log("junm");
         gravity = -17;
         onGround = false;
     }
@@ -98,165 +107,18 @@ function KeyUp(event) {
     }
 }
 
-var oldTopCollidedX = 0;
-var oldTopCollidedY = 0;
-var oldBotCollidedX = 0;
-var oldBotCollidedY = 0;
 
-function collision() {
-    var topLeftX = (playerX-(playerX % tile)) / tile;
-    var topLeftY = (playerY-(playerY % tile)) / tile;
-    var topLeftTile = tilemapV[topLeftY][topLeftX];
-
-    var botLeftX = (playerX-(playerX % tile)) / tile;
-    var botLeftY = ((playerY + playerHeight +0.001)-((playerY + playerHeight +0.001) % tile)) / tile;
-    var botLeftTile = tilemapV[botLeftY][botLeftX];
-
-    var botLeftX2 = ((playerX-(playerX % tile))) / tile;
-    var botLeftY2 = ((playerY + playerHeight - 5.001)-((playerY + playerHeight - 5.001) % tile)) / tile;
-    var botLeftTile2 = tilemapV[botLeftY2][botLeftX2];
-
-    var botRightX = ((playerX + playerWidth)-((playerX + playerWidth) % tile)) / tile;
-    var botRightY = ((playerY + playerHeight +0.001)-((playerY + playerHeight +0.001) % tile)) / tile;
-    var botRightTile = tilemapV[botRightY][botRightX];
-
-    var botRightX2 = ((playerX + playerWidth)-((playerX + playerWidth) % tile)) / tile;
-    var botRightY2 = ((playerY + playerHeight - 5.001)-((playerY + playerHeight - 5.001) % tile)) / tile;
-    var botRightTile2 = tilemapV[botRightY2][botRightX2];
-
-    var topRightX = ((playerX + playerWidth)-((playerX + playerWidth) % tile)) / tile;
-    var topRightY = (playerY-(playerY % tile)) / tile;
-    var topRightTile = tilemapV[topRightY][topRightX];
-
-    var topCollidedX = 0;
-    var topCollidedY = 0;
-    var botCollidedX = 0;
-    var botCollidedY = 0;
-
-    //neptej se, jsem retard
-    if(!topCollision("botLeft", botLeftTile)){
-        rightCollision();
-    }
-    if(!topCollision("botRight", botRightTile)){
-        leftCollision();
-    }
-    if(!rightCollision()) { botCollision("topRight", topRightTile);}
-    if(leftCollision()) {   botCollision("topLeft", topLeftTile);}
-    
-    if (botLeftTile%10000 == 0 && botRightTile%10000 == 0) { onGround = false;} //padání (detekce bloků pod hráčem)
-    
-    function botCollision(playerTile, index) {
-        if (gravity < 0) {
-            if (index%100 >= 10) {
-                if (playerTile === "topRight") {
-                console.log(topRightY*(tile+2));
-                console.log(oldPlayerY);
-                    //if ((topRightY*(tile+2)) > oldPlayerY) {
-                        playerY += tile - (playerY % tile);
-                        topCollidedX = topRightX;
-                        topCollidedY = topRightY;
-                        return true;
-                    //}
-                }else if (playerTile === "topLeft") {
-                    playerY += tile - (playerY % tile);
-                    topCollidedX = topLeftX;
-                    topCollidedY = topLeftY;
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    function topCollision(playerTile, index) { //pokud byla kolize provedena vrací true
-        if (gravity > 0) {//jinak false
-            if ((index % 10)>=1) {
-                if(playerTile === "botLeft") {
-                    if((botLeftY)*tile > oldPlayerY + playerHeight) {
-                        playerY -= (playerY+playerHeight) % tile;
-                        gravity = 0;
-                        onGround = true;
-                        botCollidedX = botLeftX;
-                        botCollidedY = botLeftY;
-                        return true;
-                    }
-                }
-                if(playerTile === "botRight"){
-                    if ((botRightY)*tile > oldPlayerY + playerHeight) {
-                        playerY -= (playerY+playerHeight) % tile;
-                        gravity = 0;
-                        onGround = true;
-                        botCollidedX = botRightX;
-                        botCollidedY = botRightY;
-                        return true;
-                    }  
-                }
-            }
-        }else if(onGround) {
-            botCollidedX = oldBotCollidedX;
-            botCollidedY = oldBotCollidedY;
-            return true;
-        }
-        return false;
-    }
-    
-    function rightCollision() {
-        if (friction < 0) {
-            if ((botLeftX != botLeftX2 || botLeftY != botLeftY2) || !onGround) {
-                if (oldPlayerX > ((botLeftX2*tile))) {
-                    if (topLeftTile%10000 >= 1000) {
-                        if (topLeftX != topCollidedX && topLeftY != topCollidedY) {
-                            playerX = topLeftX*tile + tile + 0.01;
-                            return true;
-                        }
-                    }else if(botLeftTile2%10000 >= 1000){
-                        if (botLeftX2 != botCollidedX && botLeftY2 != botCollidedY) {
-                            playerX = botLeftX2*tile + tile + 0.01;
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    function leftCollision() {
-        if (friction > 0) {
-           if (((botRightX != botRightX2) || (botRightY != botRightY2)) || !onGround) {
-               if (oldPlayerX+playerWidth< (topRightX+2)*tile - 0.01 - tile) {
-                   if ((topRightTile%1000 >=100)) {
-                        if (topRightX != topCollidedX && topRightX != topCollidedY) {
-                            playerX = topRightX*tile - 0.01 - tile;
-                            return true;
-                        }
-                   }else if(botRightTile2%1000 >=100){
-                        if(botRightX2 != botCollidedX && botRightY2 != botCollidedY){
-                        playerX = botRightX2*tile - 0.01 - tile;
-                        return true;
-                        }
-                   }
-               }
-           }
-        }
-        return false;
-    }
-    if (topCollidedX != 0) {
-    console.log(topCollidedX + " " + topCollidedY + "   " + topRightX + " " + topRightY);    
-    }
-    
-    console.log();
-    oldBotCollidedX = botCollidedX;
-    oldBotCollidedY = botCollidedY;
-    oldTopCollidedX = topCollidedX;
-    oldTopCollidedY = topCollidedY;
-}
 function draw() { //loop co běží na kolik hertzů je monitor (60/144 převážně)
     c.clearRect(0, 0, canvas.width, canvas.height);
+
+    c.fillStyle = "#D4FFE6";
+    c.fillRect(0, 0, canvas.width, canvas.height);
 
     c.fillStyle = "blue";
     c.fillRect(playerX, playerY, playerWidth, playerHeight);
 
     tilemap();
+
     window.requestAnimationFrame(draw);
 }
 function mainLoop() { // loop co běží na 60 FPS (o něco víc actually ale chápeš)
