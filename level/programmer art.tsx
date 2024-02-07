2e1130ae5493b8176cf1300ecf16a3f3d96a94ee
Damián Roháček <rohacekdamian@seznam.cz>
2024-02-07 08:41:18 +0100

    na dělání map

---
+++ level/programmer art.tsx
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="programmer art" tilewidth="50" tileheight="50" tilecount="25" columns="5">
+ <image source="C:/Users/Damiska/Desktop/programy/Aseprite.v1.2.40/Sprite-0001.png" width="250" height="250"/>
+ <tile id="0">
+  <properties>
+   <property name="name" type="int" value="10000"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.25887" y="10.052" width="33.2053" height="22.0239" visible="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" type="int" value="10001"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" type="int" value="10010"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" type="int" value="10100"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" type="int" value="11000"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" type="int" value="11111"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" type="int" value="11001"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="name" type="int" value="10101"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="name" type="int" value="11100"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="name" type="int" value="10110"/>
+  </properties>
+ </tile>
+</tileset>
