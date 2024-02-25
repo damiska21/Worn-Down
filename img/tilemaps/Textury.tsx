fc1c628400525795932a3b0b9f8c1870e442a234
Damián Roháček <rohacekdamian@seznam.cz>
2024-02-25 23:50:21 +0100

    válek release
    bez bordelu

--- img/tilemaps/Textury.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Textury" tilewidth="50" tileheight="50" tilecount="1280" columns="32">
- <image source="tilemap_vykresleni.png" width="1600" height="2000"/>
-</tileset>
--- img/tilemaps/tiledTextury.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tiledTextury" tilewidth="50" tileheight="50" tilecount="100" columns="5">
- <image source="tiledTextury.png" width="250" height="1000"/>
-</tileset>
